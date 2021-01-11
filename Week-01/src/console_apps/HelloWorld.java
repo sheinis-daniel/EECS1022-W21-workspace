7c4b4b5533ae0522a0027525756a1532e6ad06bb
sheinis-daniel <77245424+sheinis-daniel@users.noreply.github.com>
2021-01-11 14:09:15 -0500

    Lab for week 1

---
+++ Week-01/src/console_apps/HelloWorld.java
@@ -0,0 +1,9 @@
+package console_apps;
+
+public class HelloWorld {
+
+	public static void main(String args[]) {
+		System.out.println("Hello EECS1022 W21!");
+		System.out.println("Hello Jackie!");
+	}
+}
