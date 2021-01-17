e96e573a2e164e0fbc4a660a222d56d30c6a0710
sheinis-daniel <77245424+sheinis-daniel@users.noreply.github.com>
2021-01-17 10:19:37 -0500

    week 1 lab starter code

---
+++ EECS1022_W21_Lab1/src/model/Utilities.java
@@ -0,0 +1,83 @@
+package model;
+
+public class Utilities {
+	
+	/*
+	 * Assumption: the return geometric sequence is of a fixed size 5. 
+	 * Therefore, it is not necessary to solve this problem using a loop,
+	 * or to use methods from a library class (e.g., Math).\
+	 * 
+	 * Input parameters:
+	 * 	- `ft` is the first term in the sequence
+	 *  - `ratio` is the common rations among terms in the sequence
+	 *  
+	 * Refer to you lab instructions for what the method should return.
+	 */
+	public static String getGeometricSequence(int ft, int ratio) {
+		String result = "";
+		
+		/* Your implementation of this method starts here. 
+		 * Recall from Week 1's tutorial videos:
+		 * 1. No System.out.println statements should appear here.
+		 * 	  Instead, an explicit, final `return` statement is placed for you.
+		 * 2. No Scanner operations should appear here (e.g., input.nextInt()).
+		 *    Instead, refer to the input parameters of this method.   
+		 */
+		
+		
+		/* Your implementation ends here. */
+		
+		return result;
+	}
+	
+	/*
+	 * Input parameters:
+	 *   - `weight` is the user's weight in pounds
+	 *   - `height` is the user's height in inches
+	 *   
+	 * Refer to you lab instructions for what the method should return. 
+	 */
+	public static double getBMI(double weight, double height) {
+		double result = 0.0;
+		
+		/* Your implementation of this method starts here. 
+		 * Recall from Week 1's tutorial videos:
+		 * 1. No System.out.println statements should appear here.
+		 * 	  Instead, an explicit, final `return` statement is placed for you.
+		 * 2. No Scanner operations should appear here (e.g., input.nextInt()).
+		 *    Instead, refer to the input parameters of this method.   
+		 */
+		
+		
+		
+		/* Your implementation ends here. */
+		
+		return result;
+	}
+	
+	/*
+	 * Assumption: input value `seconds` is non-negative (i.e., >= 0).
+	 * 
+	 * Input parameters:
+	 *   - `seconds` is the number of seconds to be converted
+	 *   
+	 * Refer to you lab instructions for what the method should return.
+	 */
+	public static String getTimeConversion(int seconds) {
+		String result = "";
+		
+		/* Your implementation of this method starts here. 
+		 * Recall from Week 1's tutorial videos:
+		 * 1. No System.out.println statements should appear here.
+		 * 	  Instead, an explicit, final `return` statement is placed for you.
+		 * 2. No Scanner operations should appear here (e.g., input.nextInt()).
+		 *    Instead, refer to the input parameters of this method.   
+		 */
+		
+		
+		
+		/* Your implementation ends here. */
+		
+		return result;
+	}
+}
---
+++ Week_01/src/console_apps/CircleApp2.java
@@ -0,0 +1,65 @@
+package console_apps;
+
+import java.util.Scanner;
+
+import model.Circle;
+
+/*
+ * This is version one of a console application
+ * we will prompt the user of radius values of two circles
+ * the application will output the areas of the two input circles
+ * this version improves version 1 by calling a utility method
+ */
+public class CircleApp2 {
+	//main method: entry point of execution
+	public static void main(String [] args) {
+		//Starting the execution of the application
+		// System.in denotes the standard input(i.e., keyboard)
+		Scanner input = new Scanner(System.in);
+		
+		//start the actual application code here
+		
+		/*
+		 * Calculate the first circle
+		 */
+		
+		
+		//Step 1: Prompt the user for the radius of the 1st circle
+		System.out.println("Enter the radius of the 1st circle:");
+		//Declare a variable to store the user input
+		//Step 2: read a floating point number from the user.
+		double radius1 = input.nextDouble();
+		//Step 3: Compute the area of the input circle accordingly
+		
+		//Change reuse formula calculation by calling the utility method
+		double area1 = Circle.getArea(radius1);
+		
+		String area1s = String.format("%.2f", area1);
+		//Step 4: Output the result back to the user
+		System.out.println("Area of circle is: "+area1s);
+		
+		/*
+		 * Calculate the second circle
+		 */
+		
+		//Step 1: Prompt the user for the radius of the 2nd circle
+		System.out.println("Enter the radius of the 2nd circle:");
+		//Declare a variable to store the user input
+		//Step 2: read a floating point number from the user.
+		double radius2 = input.nextDouble();
+		//Step 3: Compute the area of the input circle accordingly
+		
+		//Change reuse formula calculation by calling the utility method
+		double area2 = Circle.getArea(radius2);
+		
+		String area2s = String.format("%.2f", area2);
+		//Step 4: Output the result back to the user
+		System.out.println("Area of circle is: "+area2s);
+		
+		
+		//end of the application code
+		
+		input.close();
+	}
+
+}
